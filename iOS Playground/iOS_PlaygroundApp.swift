4c3adf79fe7f9eb85c972ee4b876b2ad9be29dbd
Binhan White <binhan.daily@gmail.com>
2024-12-15 18:49:52 +0800

    init iOS demo

---
+++ iOS Playground/iOS_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_PlaygroundApp.swift
+//  iOS Playground
+//
+//  Created by hancock on 2024/12/15.
+//
+
+import SwiftUI
+
+@main
+struct iOS_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
